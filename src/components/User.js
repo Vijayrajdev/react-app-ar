e49a083bdcd6e378a52add9c5c20b4ac20fa36fc
Vijayraj-wd <vijayrajck@outlook.com>
2021-11-02 20:19:21 +0530

    Initial commit

---
+++ src/components/User.js
@@ -0,0 +1,62 @@
+import React from "react";
+
+function User(props) {
+  return (
+    <div>
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+    </div>
+  );
+}
+
+export default User;
+
+
+
+
+//* Class components
+
+// class User extends React.Component {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       planet: "Earth",
+//     };
+//     console.log("Hey I'm from constructor");
+//   }
+
+//   componentDidMount() {
+//     this.setState({ planet: "Jupiter" });
+//     console.log("Hey I'm from componentDidMount");
+//   }
+
+//   shouldComponentUpdate(nextProp, nextState) {
+//     console.log("Hey I'm from shouldComponentUpdate");
+//     console.log({ nextState, nextProp });
+//     return true;
+//   }
+
+//   getSnapshotBeforeUpdate(prevProp, prevState) {
+//     console.log("Hey I'm from getSnapshotbeforeUpdate");
+//     console.log({ prevState, prevProp });
+//     return true;
+//   }
+
+//   componentDidUpdate() {
+//     console.log(this.state);
+//   }
+
+//   render() {
+//     console.log("Hey I'm from rendered");
+//     return (
+//       <div>
+//         <h2>{this.props.name}</h2>
+//         <p>{this.props.description}</p>
+//         <h4>{this.state.planet}</h4>
+//       </div>
+//     );
+//   }
+// }
+
+// export default User;
